2d9218aa0a012603827af4a2aa4a648005f8a828
yalanskyyalexey <yalanskyyalexey@gmail.com>
2021-11-04 14:50:08 +0200

    Updates

---
+++ static/js/main.6a2e1fd3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-03-phonebook"]=this["webpackJsonpgoit-react-hw-03-phonebook"]||[]).push([[0],{10:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),r=n(3),s=n.n(r),i=(n(8),n(0));var a=function(){return Object(i.jsx)("div",{className:"App"})};s.a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.6a2e1fd3.chunk.js.map
